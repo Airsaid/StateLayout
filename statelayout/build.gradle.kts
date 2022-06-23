704efa96f73bee1333953350ab4748638c143f01
Airsaid <airsaid1024@gmail.com>
2022-06-23 16:53:13 +0800

    Bump v1.1.0

--- statelayout/build.gradle.kts
+++ statelayout/build.gradle.kts
@@ -1,12 +1,7 @@
 plugins {
   id("com.android.library")
-  id("maven-publish")
-  id("signing")
+  id("com.vanniktech.maven.publish")
 }
-
-val publishId = "multistatelayout"
-val publishVersion = "1.0.1"
-val isReleaseBuild = !publishVersion.endsWith("-SNAPSHOT")
 
 android {
   compileSdk = Versions.App.COMPILE_SDK
@@ -36,70 +31,4 @@ dependencies {
   androidTestImplementation(Libs.Test.ANDROIDX_JUNIT)
   androidTestImplementation(Libs.Test.ESPRESSO)
   implementation(Libs.AndroidX.ANNOTATION)
-}
-
-val sourcesJar by tasks.creating(Jar::class) {
-  group = JavaBasePlugin.DOCUMENTATION_GROUP
-  description = "Assembles sources JAR"
-  archiveClassifier.set("sources")
-  from(android.sourceSets.getByName("main").java.srcDirs)
-}
-
-afterEvaluate {
-  publishing {
-    publications {
-      create<MavenPublication>("release") {
-        group = "com.airsaid"
-        artifactId = publishId
-        version = publishVersion
-
-        from(components.getByName("release"))
-
-        artifact(sourcesJar)
-
-        pom {
-          name.set(publishId)
-          description.set("A customize multiple state layout for Android.")
-          url.set("https://github.com/Airsaid/MultiStateLayout")
-
-          licenses {
-            license {
-              name.set("The Apache License, Version 2.0")
-              url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
-            }
-          }
-
-          developers {
-            developer {
-              id.set(property("pom.id") as String)
-              name.set(property("pom.name") as String)
-              email.set(property("pom.email") as String)
-            }
-          }
-
-          scm {
-            connection.set("scm:git:git://github.com/Airsaid/MultiStateLayout.git")
-            developerConnection.set("scm:git:ssh://git@github.com/Airsaid/MultiStateLayout.git")
-            url.set("https://github.com/Airsaid/MultiStateLayout")
-          }
-        }
-      }
-    }
-
-    repositories {
-      maven {
-        setUrl(if (isReleaseBuild) "https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/"
-        else "https://s01.oss.sonatype.org/content/repositories/snapshots/")
-
-        credentials {
-          username = if (hasProperty("ossrhUsername")) properties["ossrhUsername"] as String else ""
-          password = if (hasProperty("ossrhPassword")) properties["ossrhPassword"] as String else ""
-        }
-      }
-    }
-  }
-}
-
-signing {
-  sign(publishing.publications)
 }
